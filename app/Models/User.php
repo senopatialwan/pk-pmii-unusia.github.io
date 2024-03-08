0dcf87c4b20bc2097d5cf070e2ff1207bcb995d9
Saeful Mu'minin <saeful2026027@gmail.com>
2024-03-08 23:05:15 +0700

    tambah persion

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,22 +2,27 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+
+use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Spatie\Permission\Traits\HasRoles;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\Casts\Attribute;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasApiTokens, HasFactory, Notifiable, HasRoles;
 
     /**
      * The attributes that are mass assignable.
      *
      * @var array<int, string>
      */
-    protected $fillable = [
+        protected $fillable = [
         'name',
         'email',
         'password',
@@ -31,6 +36,8 @@ class User extends Authenticatable
     protected $hidden = [
         'password',
         'remember_token',
+        'two_factor_secret',
+        'two_factor_recovery_codes'
     ];
 
     /**
@@ -40,6 +47,5 @@ class User extends Authenticatable
      */
     protected $casts = [
         'email_verified_at' => 'datetime',
-        'password' => 'hashed',
     ];
 }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\AdminController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -26,21 +26,32 @@ Route::view('about', 'pages.about')->name('about');
 Route::view('database', 'pages.database')->name('database');
 Route::view('produk-hukum', 'pages.produk-hukum')->name('produk-hukum');
 
-// *Admin
-Route::view('admin/dashboard', 'admin.dashboard')->name('admin.dashboard');
+// // *Admin
+// Route::view('admin/dashboard', 'admin.dashboard')->name('admin.dashboard');
 
-// *database
-Route::view('admin/database/anggota-PMII', 'admin.database.index')->name('admin.database.index');
-Route::view('admin/database/verivikasi-kta', 'admin.database.verivikasi-kta')->name('admin.database.verivikasi-kta');
-Route::view('admin/database/tambah-anggota', 'admin.database.create')->name('admin.database.create');
-Route::view('admin/database/edit-anggota', 'admin.database.edit')->name('admin.database.edit');
-Route::view('admin/database/detail-anggota', 'admin.database.show')->name('admin.database.show');
+// // *database
+// Route::view('admin/database/anggota-PMII', 'admin.database.index')->name('admin.database.index');
+// Route::view('admin/database/verivikasi-kta', 'admin.database.verivikasi-kta')->name('admin.database.verivikasi-kta');
+// Route::view('admin/database/tambah-anggota', 'admin.database.create')->name('admin.database.create');
+// Route::view('admin/database/edit-anggota', 'admin.database.edit')->name('admin.database.edit');
+// Route::view('admin/database/detail-anggota', 'admin.database.show')->name('admin.database.show');
 
-// *pengurus
-Route::view('admin/struktur-organisasi/data-Pengurus', 'admin.struktur-organisasi.index')->name('admin.struktur-organisasi.index');
-Route::view('admin/struktur-organisasi/tambah-Pengurus', 'admin.struktur-organisasi.create')->name('admin.struktur-organisasi.create');
-Route::view('admin/struktur-organisasi/edit-Pengurus', 'admin.struktur-organisasi.edit')->name('admin.struktur-organisasi.edit');
+// // *pengurus
+// Route::view('admin/struktur-organisasi/data-Pengurus', 'admin.struktur-organisasi.index')->name('admin.struktur-organisasi.index');
+// Route::view('admin/struktur-organisasi/tambah-Pengurus', 'admin.struktur-organisasi.create')->name('admin.struktur-organisasi.create');
+// Route::view('admin/struktur-organisasi/edit-Pengurus', 'admin.struktur-organisasi.edit')->name('admin.struktur-organisasi.edit');
 
 
 
+Route::group(['as' => 'admin.', 'prefix' => 'admin', 'middleware' => ['auth', 'role:admin']], function () {
+    Route::get('/', [AdminController::class, 'dashboard'])->name('dashboard');
+    Route::get('/database/anggota-PMII', [AdminController::class, 'showDatabaseIndex'])->name('database.index');
+    Route::get('/database/verifikasi-kta', [AdminController::class, 'showVerifikasiKta'])->name('database.verifikasi-kta');
+    Route::get('/database/tambah-anggota', [AdminController::class, 'showCreateAnggota'])->name('database.create');
+    Route::get('/database/edit-anggota', [AdminController::class, 'showEditAnggota'])->name('database.edit');
+    Route::get('/database/detail-anggota', [AdminController::class, 'showDetailAnggota'])->name('database.show');
+    Route::get('/struktur-organisasi/data-Pengurus', [AdminController::class, 'showStrukturOrganisasiIndex'])->name('struktur-organisasi.index');
+    Route::get('/struktur-organisasi/tambah-Pengurus', [AdminController::class, 'showCreatePengurus'])->name('struktur-organisasi.create');
+    Route::get('/struktur-organisasi/edit-Pengurus', [AdminController::class, 'showEditPengurus'])->name('struktur-organisasi.edit');
+});
 
