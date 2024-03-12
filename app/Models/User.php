ebee8d726005745bd157825811da6f298af47d5f
Syahravi <cc@syahravi.my.id>
2024-03-12 11:53:56 +0700

    zen
    Signed-off-by: Syahravi <cc@syahravi.my.id>

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,17 +2,16 @@
 
 namespace App\Models;
 
-
-
-use Illuminate\Contracts\Auth\MustVerifyEmail;
-
+// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 use App\Traits\Uuids;
 
 class User extends Authenticatable
 {
-    use Notifiable, Uuids;
+    use HasApiTokens, HasFactory, Notifiable, Uuids;
 
     /**
      * The attributes that are mass assignable.
@@ -43,5 +42,6 @@ class User extends Authenticatable
      */
     protected $casts = [
         'email_verified_at' => 'datetime',
+        'password' => 'hashed',
     ];
 }
