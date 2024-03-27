5df8fedd9adf0430e93fec1b376a7e7890ab962f
Syahravi <cc@syahravi.my.id>
2024-03-27 17:02:11 +0700

    feat: AnggotaController
    Signed-off-by: Syahravi <cc@syahravi.my.id>

--- routes/web.php
+++ routes/web.php
@@ -10,16 +10,6 @@ use App\Http\Controllers\Admin\TagController;
 use App\Http\Controllers\Admin\BlogController;
 use App\Http\Controllers\Pages\CetakKTAController;
 use App\Http\Controllers\Pages\PagesBlogController;
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
 
 // Route::get('/', function () {
 //     return view('welcome');
