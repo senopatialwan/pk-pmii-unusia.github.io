a0a0deb9171d7341c3fc0ab1bf14cef04e527525
alwanabdurrahman <alwanabdurrahman095@gmail.com>
2024-03-27 16:50:24 +0700

    temp:button "Download PDF"

--- resources/views/pages/id-cetak-kta.blade.php
+++ resources/views/pages/id-cetak-kta.blade.php
@@ -11,7 +11,6 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title>KTA | PMII UNUSIA BOGOR</title>
 <style>
-
 .output {
     display: flex;
     justify-content: center;
@@ -26,7 +25,6 @@
   width: 412.5px;
   height: 250px;
   margin-bottom: 1rem;
-  
   border-radius: 10px;
   overflow: hidden;
   box-shadow: 0 0 5px rgba(0,0,0, 0.5);
@@ -73,28 +71,46 @@
   opacity: 0;
   visibility: hidden;
 }
+
+.download-btn {
+    background-color: #4CAF50;
+    color: white;
+    padding: 10px 20px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 16px;
+    border-radius: 10px;
+    border: none;
+    cursor: pointer;
+  }
+
+.download-btn:hover {
+    background-color: #45a049;
+  }
 </style>
 </head>
 <body>
   <main id="main" class="output">
-  <div class="ac-card">
-    <div class="ac-card-image">
-    <img src="assets/img/pmii/alwan.jpg" alt="Thumb">
+    <div class="ac-card">
+      <div class="ac-card-image">
+      <img src="assets/img/pmii/alwan.jpg" alt="Thumb">
+      </div>
+      <div class="ac-card-info">
+        <p><strong id="name">Muhamad Alwan Abdurrahman</strong></p>
+        <p id="studentNumber">2025005</p>
+        <p id="pengurus">PK PMII UNUSIA BOGOR</p>
+        <p id="fakultas">FTIK - Sistem Informasi</p>
+        <p id="domisili">Domisili - Bogor</p>
+        <p id="angkatanMapaba">Angkatan Mapaba - 2020</p>
+      </div>
+      <img class="ac-icon" src="assets/img/pmii/logo-pmii.png">
+      <div class="ac-card-footer">
+        <img class="ac-logo" src="assets/img/pmii/logo-pmii.png">
+      </div>
     </div>
-    <div class="ac-card-info">
-      <p><strong id="name">Muhamad Alwan Abdurrahman</strong></p>
-      <p id="studentNumber">2025005</p>
-      <p id="pengurus">PK PMII UNUSIA BOGOR</p>
-      <p id="fakultas">FTIK - Sistem Informasi</p>
-      <p id="domisili">Domisili - Bogor</p>
-      <p id="angkatanMapaba">Angkatan Mapaba - 2020</p>
-    </div>
-    <img class="ac-icon" src="assets/img/pmii/logo-pmii.png">
-    <div class="ac-card-footer">
-      <img class="ac-logo" src="assets/img/pmii/logo-pmii.png">
-    </div>
-  </div>
-    </main>
+  </main>
+  <button class="download-btn">Download PDF</button>
 </body>
 </html>
 
