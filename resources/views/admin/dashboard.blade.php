e60d2d201bcb96afc44fef3d782209e213103831
syahravi <syahravi.id@gmail.com>
2024-05-19 16:58:34 +0700

    fix: dashboard

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -20,34 +20,85 @@
                 <div class="row">
                     <div class="col-md-3 col-sm-6 col-xs-12">
                         <div class="r4_counter db_box">
+                            <i class="pull-left fa fa-users icon-md icon-rounded icon-primary"></i>
+                            <div class="stats">
+                                <h4>
+                                    <strong>{{ $userCount }}</strong>
+                                </h4>
+                                <span>Total Users</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-3 col-sm-6 col-xs-12">
+                        <div class="r4_counter db_box">
                             <i class="pull-left fa fa-user icon-md icon-rounded icon-orange"></i>
                             <div class="stats">
                                 <h4>
-                                    <strong>6243</strong>
+                                    <strong>{{ $blogCount }}</strong>
                                 </h4>
-                                <span>Total Pengunjung</span>
+                                <span>Total Blogs</span>
                             </div>
                         </div>
                     </div>
                     <div class="col-md-3 col-sm-6 col-xs-12">
                         <div class="r4_counter db_box">
-                            <i class="pull-left fa fa-database icon-md icon-rounded icon-purple"></i>
+                            <i class="pull-left fa fa-user icon-md icon-rounded icon-orange"></i>
                             <div class="stats">
                                 <h4>
-                                    <strong>1000</strong>
+                                    <strong>{{ $anggotaCount }}</strong>
                                 </h4>
-                                <span>Data Anggota PMII UNUSIA</span>
+                                <span>Total Anggota</span>
                             </div>
                         </div>
                     </div>
                     <div class="col-md-3 col-sm-6 col-xs-12">
                         <div class="r4_counter db_box">
-                            <i class="pull-left fa fa-envelope icon-md icon-rounded icon-warning"></i>
+                            <i class="pull-left fa fa-user icon-md icon-rounded icon-orange"></i>
                             <div class="stats">
                                 <h4>
-                                    <strong>20</strong>
+                                    <strong>{{ $pengurusCount }}</strong>
                                 </h4>
-                                <span>KTA Belum Terverifikasi</span>
+                                <span>Total Pengurus</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="r4_counter db_box">
+                            <div class="stats">
+                                <h4>
+                                    <strong>Total blog</strong>
+                                </h4>
+                                <canvas id="blogChart"></canvas>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="r4_counter db_box">
+                            <div class="stats">
+                                <h4>
+                                    <strong>Total admin</strong>
+                                </h4>
+                                <canvas id="adminChart"></canvas>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="r4_counter db_box">
+                            <div class="stats">
+                                <h4>
+                                    <strong>Total Anggota</strong>
+                                </h4>
+                                <canvas id="anggotaChart"></canvas>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="r4_counter db_box">
+                            <div class="stats">
+                                <h4>
+                                    <strong>Total Pengurus</strong>
+                                </h4>
+                                <canvas id="pengurusChart"></canvas>
                             </div>
                         </div>
                     </div>
@@ -55,4 +106,98 @@
             </div>
         </section>
     </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+    <script>
+          var blogChartCanvas = document.getElementById("blogChart");
+        var adminChartCanvas = document.getElementById("adminChart");
+
+        var blogChartData = {
+            labels: {!! $blogChartData->keys() !!},
+            datasets: [{
+                label: 'Blogs',
+                data: {!! $blogChartData->values() !!},
+                backgroundColor: 'rgba(153, 246, 228, 0.2)',
+                borderColor: 'rgba(20, 184, 166, 1)',
+
+                borderWidth: 1
+            }]
+        };
+
+        var adminChartData = {
+            labels: {!! $userChartData->keys() !!},
+            datasets: [{
+                label: 'Admins',
+                data: {!! $userChartData->values() !!},
+                backgroundColor: 'rgba(255, 206, 86, 0.2)',
+                borderColor: 'rgba(255, 206, 86, 1)',
+                borderWidth: 1
+            }]
+        };
+
+        var blogChart = new Chart(blogChartCanvas, {
+            type: 'bar',
+            data: blogChartData,
+            options: {
+                scales: {
+                    y: {
+                        beginAtZero: true
+                    }
+                }
+            }
+        });
+
+        var adminChart = new Chart(adminChartCanvas, {
+            type: 'doughnut',
+            data: adminChartData
+        });
+        var anggotaChartCanvas = document.getElementById("anggotaChart");
+        var pengurusChartCanvas = document.getElementById("pengurusChart");
+
+        var anggotaChartData = {
+            labels: {!! $anggotaChartData->keys() !!},
+            datasets: [{
+                label: 'Anggota',
+                data: {!! $anggotaChartData->values() !!},
+                backgroundColor: 'rgba(255, 99, 132, 0.2)',
+                borderColor: 'rgba(255, 99, 132, 1)',
+                borderWidth: 1
+            }]
+        };
+
+        var pengurusChartData = {
+            labels: {!! $pengurusChartData->keys() !!},
+            datasets: [{
+                label: 'Pengurus',
+                data: {!! $pengurusChartData->values() !!},
+                backgroundColor: 'rgba(54, 162, 235, 0.2)',
+                borderColor: 'rgba(54, 162, 235, 1)',
+                borderWidth: 1
+            }]
+        };
+
+        var anggotaChart = new Chart(anggotaChartCanvas, {
+            type: 'line',
+            data: anggotaChartData,
+            options: {
+                scales: {
+                    y: {
+                        beginAtZero: true
+                    }
+                }
+            }
+        });
+
+        var pengurusChart = new Chart(pengurusChartCanvas, {
+            type: 'bar',
+            data: pengurusChartData,
+            options: {
+                scales: {
+                    y: {
+                        beginAtZero: true
+                    }
+                }
+            }
+        });
+    </script>
 @endsection
